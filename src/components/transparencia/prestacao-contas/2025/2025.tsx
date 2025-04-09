249bbb510442463b9332204dd467a722e311fd8d
Leonardo Batista <acippcasaabrigo40@gmail.com>
2025-04-09 10:32:30 -0300

    Feature: new documents on transparencia 2025

--- src/components/transparencia/prestacao-contas/2025/2025.tsx
+++ src/components/transparencia/prestacao-contas/2025/2025.tsx
@@ -8,6 +8,16 @@ import PDF1 from "../../../../assets/transparencia/2025/TA-06-2025.pdf";
 import PDF2 from "../../../../assets/transparencia/2025/TA-07-2025.pdf";
 import PDF3 from "../../../../assets/transparencia/2025/TF-01-2022.pdf";
 import PDF4 from "../../../../assets/transparencia/2025/TF-05-2022.pdf";
+
+import PDF5 from "../../../../assets/transparencia/2025/TF 02-2022 | TA 08-2025/TF 02.2022.pdf";
+import PDF6 from "../../../../assets/transparencia/2025/TF 02-2022 | TA 08-2025/TA 08.2025.pdf";
+
+import PDF7 from "../../../../assets/transparencia/2025/TF 03-2022 | TA 21-2025/TF 03.2022.pdf";
+import PDF8 from "../../../../assets/transparencia/2025/TF 03-2022 | TA 21-2025/TA 21.2025.pdf";
+
+import PDF9 from "../../../../assets/transparencia/2025/TF 04-2022 | TA 05-2025/TF 04.2022.pdf";
+import PDF10 from "../../../../assets/transparencia/2025/TF 04-2022 | TA 05-2025/TA 05.2025.pdf";
+
 import { MdOutlinePictureAsPdf } from "react-icons/md";
 
 export function Doc2025() {
@@ -38,6 +48,51 @@ export function Doc2025() {
         {
           title: "TA 07/2025",
           link: PDF2,
+          newTab: true,
+        },
+      ],
+    },
+    {
+      name: "TF 02/2022 | TA 08/2025",
+      documents: [
+        {
+          title: "TF 02/2022",
+          link: PDF5,
+          newTab: true,
+        },
+        {
+          title: "TA 08/2025",
+          link: PDF6,
+          newTab: true,
+        },
+      ],
+    },
+    {
+      name: "TF 03/2022 | TA 21/2025",
+      documents: [
+        {
+          title: "TF 03/2022",
+          link: PDF7,
+          newTab: true,
+        },
+        {
+          title: "TA 21/2025",
+          link: PDF8,
+          newTab: true,
+        },
+      ],
+    },
+    {
+      name: "TF 04/2022 | TA 05/2025",
+      documents: [
+        {
+          title: "TF 04/2022",
+          link: PDF9,
+          newTab: true,
+        },
+        {
+          title: "TA 05/2025",
+          link: PDF10,
           newTab: true,
         },
       ],
