172fef61609f06ef0e0451ed8f295a5a884e6503
lmbbetetto <lmbbetetto@gmail.com>
2026-01-31 10:04:44 -0300

    Feature: correcao home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef } from "react";
 import imgFundo from "../../assets/fundo_home.jpg";
 
 import { Link } from "react-router-dom";
@@ -10,13 +10,13 @@ import { noticias } from "./mock";
 import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
 
 export function Home() {
-  const [showModal, setShowModal] = useState(false);
+  // const [showModal, setShowModal] = useState(false);
 
   const carouselRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    setShowModal(true);
-  }, []);
+  // useEffect(() => {
+  //   setShowModal(true);
+  // }, []);
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -24,37 +24,60 @@ export function Home() {
 
   return (
     <>
-      {showModal && <Modal onClose={() => setShowModal(false)} />}
+      {/* {showModal && <Modal onClose={() => setShowModal(false)} />} */}
       <div className="img_fundo">
         <img src={imgFundo} alt="Imagem de fundo" className="imgHome" />
       </div>
-      <div className="sobreHome">
-        <div className="titulo">
-          <h1>História</h1>
+
+      <section className="sec_home">
+        <div className="sobreHome">
+          <div className="titulo">
+            <h1>História</h1>
+          </div>
+          <p>
+            A organização Casa Lar foi fundada em 10 de dezembro de 1958 com o
+            objetivo de oferecer proteção integral a crianças e adolescentes em
+            situação de abandono e desamparo familiar. Ao longo do tempo, a Casa
+            Lar demonstrou extrema preocupação com o bem-estar desses jovens. Em
+            2006, a organização se tornou a Organização Certificadora do Projeto
+            C.A.R.A. (Construindo Ações Reais para Adolescentes), que desde 2004
+            desenvolve o Serviço de Convivência e Fortalecimento de Vínculos
+            para Jovens, com foco na qualificação e inserção no mundo do
+            trabalho, conforme a Lei do Aprendiz nº. 10.097/2000.
+          </p>
+          <p>
+            No ano de 2025 se tornou a Organização Certificadora do Centro de
+            Estimulação e Reabilitação Precoce
+          </p>
+          <div className="divbotao">
+            <Link to="/sobre">
+              <button className="btn_noticia" onClick={scrollToTop}>
+                História completa
+              </button>
+            </Link>
+          </div>
         </div>
-        <p>
-          A organização Casa Lar foi fundada em 10 de dezembro de 1958 com o
-          objetivo de oferecer proteção integral a crianças e adolescentes em
-          situação de abandono e desamparo familiar. Ao longo do tempo, a Casa
-          Lar demonstrou extrema preocupação com o bem-estar desses jovens. Em
-          2006, a organização se tornou a Organização Certificadora do Projeto
-          C.A.R.A. (Construindo Ações Reais para Adolescentes), que desde 2004
-          desenvolve o Serviço de Convivência e Fortalecimento de Vínculos para
-          Jovens, com foco na qualificação e inserção no mundo do trabalho,
-          conforme a Lei do Aprendiz nº. 10.097/2000.
-        </p>
-        <p>
-          No ano de 2025 se tornou a Organização Certificadora do Centro de
-          Estimulação e Reabilitação Precoce
-        </p>
-        <div className="divbotao">
-          <Link to="/sobre">
-            <button className="btn_noticia" onClick={scrollToTop}>
-              História completa
-            </button>
-          </Link>
-        </div>
-      </div>
+
+        <section className="transparenciaHome">
+          <div className="internaHome transparenciaBox">
+            <div className="titulo">
+              <h1>Transparência</h1>
+            </div>
+
+            <p>
+              A Casa Lar tem a transparência como um princípio, por isso,
+              disponibilizamos nossos relatórios, balanços e convênios com o
+              poder público!
+            </p>
+
+            <div className="divbotao-transp">
+              <Link to="/transparencia">
+                <button className="btn_noticia">Transparência</button>
+              </Link>
+            </div>
+          </div>
+        </section>
+      </section>
 
       <section className="container2">
         {/* NOTÍCIAS */}
@@ -100,27 +123,6 @@ export function Home() {
               >
                 <IconChevronRight />
               </button>
-            </div>
-          </div>
-        </section>
-
-        {/* TRANSPARÊNCIA */}
-        <section className="transparenciaHome">
-          <div className="internaHome transparenciaBox">
-            <div className="titulo">
-              <h1>Transparência</h1>
-            </div>
-
-            <p>
-              A Casa Lar tem a transparência como um princípio, por isso,
-              disponibilizamos nossos relatórios, balanços e convênios com o
-              poder público!
-            </p>
-
-            <div className="divbotao">
-              <Link to="/transparencia">
-                <button className="btn_noticia">Transparência</button>
-              </Link>
             </div>
           </div>
         </section>
